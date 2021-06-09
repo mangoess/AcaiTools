f478c4556d891d03801b1dc731418362378e327a
mango <adamtamers@outlook.com>
2021-06-09 08:20:21 +0400

    Update mouselocation.py

--- MouseLocation/mouselocation.py
+++ MouseLocation/mouselocation.py
@@ -1,12 +1,12 @@
-import pyautogui
-import time
-from datetime import datetime
+import pyautogui # To find the location of the position of your mouse
+import time # Just to time
+from datetime import datetime # The module to get the date / time that is in the message
 
-print("5 Second Timer till location outputted")
-time.sleep(1)
-print("5 Seconds completed, Starting program")
+print("3 Second Timer till location outputted")
+time.sleep(3)
+print("3 Seconds completed, Starting program")
 while True:
-    time.sleep(2)
+    time.sleep(2) # Updates and gives a new position every 2 seconds.
     location = CurrentMouseX, currentMouseY = pyautogui.position()
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
